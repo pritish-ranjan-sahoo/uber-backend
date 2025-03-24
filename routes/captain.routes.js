a6027bb305953f7f03f4ee6fd3aa8147c3328df8
Pritish <codecharlie404@gmail.com>
2025-03-24 22:00:11 +0530

    Initial Commit

---
+++ routes/captain.routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router();
+const { body } = require('express-validator')
+const captainController = require('../controllers/captain.controller')
+const authMiddleware = require('../middlewares/auth.middleware')
+
+router.post('/register',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min:3}).withMessage('First name must be atleast 3 characters long'),
+    body('vehicle.color').isLength({min:3}).withMessage('vehicle color must be atleast 3 characters long'),
+    body('vehicle.plate').isLength({min:3}).withMessage('vehicle plate must be atleast 3 characters long'),
+    body('vehicle.capacity').isInt({min:1}).withMessage('capacity must be atleast 1'),
+    body('vehicle.vehicleType').isIn(['car','motorcycle','auto']).withMessage('Invalid Vehicle'),
+],captainController.registerCaptain)
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:6}).withMessage('Password must be atleast 6 characters long')
+],captainController.loginCaptain)
+
+router.get('/profile',authMiddleware.authCaptain,captainController.getCaptainProfile)
+router.get('/logout',authMiddleware.authCaptain,captainController.logoutCaptain)
+
+module.exports = router;
